6c67b4e2a487f5fc06706e8a9fdb58b453c00426
Reinawan <admin@reinawan.com>
2024-01-02 21:45:26 +0700

    feat: seeders categories

---
+++ seeders/20240102144332-seed-categories.js
@@ -0,0 +1,34 @@
+'use strict';
+const fs = require('fs');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    let data = JSON.parse(fs.readFileSync('./data/categories.json','utf-8'))
+    .map((item) => {
+      item.createdAt = item.updatedAt = new Date();
+      return item;
+    })
+    // console.log(data);
+    await queryInterface.bulkInsert('Categories', data);
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Categories', null, {});
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
